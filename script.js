b1baf4ba5405ceaf5aaf6a58ec5799822db8c95b
PieStw <piero.stalteri@icloud.com>
2024-11-06 14:05:26 +0100

    aggiunta grafica

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ const emailList = [];
 
 
 function getEmail(){
+  document.getElementById("email-container").innerHTML = ``;
   for(let i = 0; i < 10; i++){
     fetch("https://flynn.boolean.careers/exercises/api/random/mail")
       .then((response) => response.json())
       .then((data) => {
         emailList.push(data.response);
-        document.getElementById("email-container").innerHTML += `<p>${data.response}</p>`;
+        document.getElementById("email-container").innerHTML += `<div>${data.response}</div>`;
       });
   }
 }
